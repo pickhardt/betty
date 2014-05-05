34569aee8e8eacb87fe886d1abe288cd4e3085da
Roman Zubenko <romanz@bu.edu>
2014-05-04 22:00:09 -0400

    Get the version of distribution.

--- lib/user.rb
+++ lib/user.rb
@@ -22,6 +22,38 @@ module User
         :explanation => "Gets your external ip address."
       }
     end
+
+    if command.match(/^what\'?s?(\s+is)?(\s+version)?(\s+of)?(\s[a-zA-Z]*)?\??$/i)
+      program = command.split("version").last
+      program = program.split("of").last
+      program = program.split("?").first
+      program = program.strip
+
+      computerCommand = ""
+      case program
+      when "python"
+        computerCommand = "python -V"
+      when "ruby"
+        computerCommand = "ruby -v"
+      when "node"
+        computerCommand = "node --version"
+      when "redis"
+        computerCommand = "redis-server -v"
+      when "mysql"
+        computerCommand = "mysql -u root -p -e ' SELECT VERSION(); '"
+      when "php"
+        computerCommand = "php -v"
+      when "mongo" || "mongoDB"
+        computerCommand = "mongo --version"
+      else
+        computerCommand = program + " -v"
+      end
+      
+      responses << {
+        :command => computerCommand,
+        :explanation => "Gets the version of distribution."
+      }
+    end
     
     responses
   end
