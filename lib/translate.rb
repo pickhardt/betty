fb60d1e00235930221e50ebdcdbb88c9e463f303
Jeff Pickhardt <pickhardt@gmail.com>
2014-05-07 08:08:02 -0700

    Added translate.

--- lib/translate.rb
+++ lib/translate.rb
@@ -1,15 +1,16 @@
 module Translate
   def self.interpret(command)
     responses = []
-    matches = command.match(/^(?:translate\s+)?(.+)(?:from\s+)(.+)(?:to\s+)(.+)$/)
+    matches = command.match(/^translate\s+(.+)\s+from\s+(.+)?(?:to\s+)(\w+)$/)
 
     if matches
       translate_string = matches[1].gsub(' ', '%20')
-      from = matches[2].strip
+      from = matches[2] || "english"
+      from = from.strip
       to = matches[3].strip
 
       responses << {
-        :command => "open https://translate.google.com.br/##{from}/#{to}/#{translate_string}",
+        :command => "open https://translate.google.com/##{from}/#{to}/#{translate_string}",
         :explanation => "Opens a browser on Google Translate translating #{translate_string} from #{from} to #{to}."
       }
     end
