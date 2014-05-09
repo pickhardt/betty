9e308a546c7b49b8cf8cf4f6bbfa35eb31db04d2
Igor Kur <i.v.kurinnoy@mail.ru>
2014-05-09 23:00:27 +0300

    Change map module to use command depend on platform

--- lib/map.rb
+++ lib/map.rb
@@ -1,16 +1,26 @@
-module Map  
+module Map
   def self.interpret(command)
     responses = []
-    
+
     if command.match(/^(?:show\s+)?(?:me\s+)?(?:a\s+)?map\s+(?:of\s+)?(.+)$/)
       search_term = $1.gsub(' ', '%20')
-      
+      command = ""
+
+      case OS.platform_name
+        when 'Mac OS'
+          command = 'open'
+        when 'Linux'
+          command = 'xdg-open'
+        when 'Windows'
+          command = 'start'
+      end
+
       responses << {
-        :command => "open https://www.google.com/maps/search/#{ search_term }",
+        :command => "#{command} https://www.google.com/maps/search/#{ search_term }",
         :explanation => "Opens a browser with Google Maps searching for '#{ search_term }'."
       }
     end
-    
+
     responses
   end
 end
