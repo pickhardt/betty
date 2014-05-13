27478cba9c8259fae33ad864ba616cc912e86db9
Muhammad Hussein Nasrollahpour <iapplechocolate@me.com>
2014-05-13 08:46:12 -0700

    Add OS documentation.

--- lib/os.rb
+++ lib/os.rb
@@ -1,20 +1,39 @@
+# os.rb
+#
+# Author: betty developer
+# Date: 2014
+# Copyright: See the license agreement
+#
+
+# task: This module determins the kind of OS that betty is running on
+# and shows the OS when user asks betty
+
 module OS
 
+  # finds the OS and returns its name
   def self.platform_name
     os = ""
 
     case RUBY_PLATFORM
-      when '/cygwin|mswin|mingw|bccwin|wince|emx/'
+      when /cygwin|mswin|mingw|bccwin|wince|emx/
         os = "Windows"
-      when '/darwin/'
-        os = "Mac OS"
-      else
+      when /darwin/
+        os = "OS X"
+      when /freebsd/
+        os = "FreeBSD"
+      when /openbsd/
+        os = "OpenBSD"
+      when /netbsd/
+        os = "NetBSD"
+      when /linux/
         os = "Linux"
     end
 
     os
   end
 
+  # Validates the question that is asked from betty
+  # Sees whether or not the question is related to OS
   def self.interpret(command)
     responses = []
 
@@ -30,6 +49,7 @@ module OS
     responses
   end
 
+  # Shows the help for the specified executer
   def self.help
     commands = []
     commands << {
