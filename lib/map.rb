1267ccbcd4cd773bfa89b73cacb2d6b22c02e47e
Christophe Lecointe <lecoin_c@epitech.eu>
2014-05-20 16:04:58 +0900

    Easier platform management and added support for translation and spotify on Linux

--- lib/map.rb
+++ lib/map.rb
@@ -4,19 +4,9 @@ module Map
 
     if command.match(/^(?:show\s+)?(?:me\s+)?(?:a\s+)?map\s+(?:of\s+)?(.+)$/)
       search_term = $1.gsub(' ', '%20')
-      command = ""
-
-      case OS.platform_name
-        when 'Mac OS'
-          command = 'open'
-        when 'Linux'
-          command = 'xdg-open'
-        when 'Windows'
-          command = 'start'
-      end
 
       responses << {
-        :command => "#{command} https://www.google.com/maps/search/#{ search_term }",
+        :command => Command.browser("https://www.google.com/maps/search/#{ search_term }"),
         :explanation => "Opens a browser with Google Maps searching for '#{ search_term }'."
       }
     end
