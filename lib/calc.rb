e73accff1213cb86ec7bbbe229ff10ac7f85caaf
Jeff Pickhardt <pickhardt@gmail.com>
2014-05-26 21:34:02 -0700

    Fix calc module.

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,61 +1,58 @@
 class String
-    def numeric?
-        Float(self) != nil rescue false
-    end
+  def numeric?
+    Float(self) != nil rescue false
+  end
 end
 
 module Calculate
-
-    def self.interpret(command)
-        responses = []
-        matches = command.match(/^what\s+is\s+(.+)\s+(.+)\s(.+)$/)
-
-        if matches
-            if (matches[1].numeric? && matches[3].numeric?)
-                arg1 = matches[1]
-                arg2 = matches[3]
-            else argerror()
-            end
-
-            oper = matches[2]
-            if oper == "plus"
-                op = "+"
-            elsif oper == "minus"
-                op = "-"
-            elsif oper == "times"
-                op = "*"
-            elsif oper == "by"
-                op = "/"
-            else operror()
-            end
-
-            responses << {
-                :command => "bc <<< #{arg1}#{op}#{arg2}",
-                :explanation => "Calculates #{arg1}#{op}#{arg2}"
-            }
-        end
-        responses
-    end
-
-
-    def self.help
-        commands = []
-        commands << {
-            :category => "Calculate",
-            :description => '\033[34mCalculate\033[0m',
-            :usage => ["- betty what is 40 plus 2"]
+  def self.interpret(command)
+    responses = []
+    matches = command.match(/^what\s+is\s+(.+)\s+(.+)\s(.+)$/)
+    
+    if matches
+      if (matches[1].numeric? && matches[3].numeric?)
+        arg1 = matches[1]
+        arg2 = matches[3]
+      else 
+        responses << {
+          say: "I'm sorry, you should use numbers. I can't calculate with apples and oranges."
         }
-        commands
+        return responses
+      end
+      
+      oper = matches[2]
+      if ["plus", "+"].include?(oper)
+        op = "+"
+      elsif ["minus", "-"].include?(oper)
+        op = "-"
+      elsif ["times", "*"].include?(oper)
+        op = "*"
+      elsif ["by", "divided", "divied by", "/"].include?(oper)
+        op = "/"
+      else
+        responses << {
+          say: "I'm sorry, I can't do complex maths yet."
+        }
+        return responses
+      end
+      
+      responses << {
+        command: "bc <<< #{arg1}#{op}#{arg2}",
+        explanation: "Calculates #{arg1}#{op}#{arg2}"
+      }
     end
-
-    def self.argerror
-        print "I think you should use numbers. I can't calculate with apples and oranges :(\n"
-        exit
-    end
-
-    def self.operror
-        print "I can't do complex maths yet :(\n"
-        exit
-    end
+    responses
+  end
+  
+  def self.help
+    commands = []
+    commands << {
+      category: "Calculate",
+      description: '\033[34mCalculate\033[0m',
+      usage: ["- betty what is 40 plus 2"]
+    }
+    commands
+  end
 end
+
 $executors << Calculate
