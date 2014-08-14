60b0c05da85da3a031d275e32f9ce13aa80c9f9a
joanjoc <joanjoc@gmail.com>
2014-08-14 19:00:18 +0200

    Added square root, square and cube

--- lib/calc.rb
+++ lib/calc.rb
@@ -7,8 +7,52 @@ end
 module Calculate
   def self.interpret(command)
     responses = []
-    matches = command.match(/^what\s+is\s+(.+)\s+(.+)\s(.+)$/)
     
+    
+    matches = command.match(/^what\s+is\s+(?:the\s+)?(?:square\s+root|sqrt)\s+of\s+(.+)$/)
+    if matches
+
+      if matches[1].numeric?
+        arg1 = matches[1]
+      else 
+        return []
+      end
+
+      responses << {
+        command: "bc <<< 'scale=10;sqrt(#{arg1})'",
+        explanation: "Calculates square root of #{arg1}"
+      }
+    return responses
+    end
+
+
+    matches = command.match(/^what\s+is\s+(?:the\s+)?(square|cube)\s+of\s+(.+)$/)
+    if matches
+
+      power_text = matches[1]
+      if power_text == 'square'
+         power = 2
+      elsif power_text == 'cube'
+         power = 3
+      else 
+        return []
+      end
+
+      if matches[2].numeric?
+        arg1 = matches[2]
+      else 
+        return []
+      end
+     
+      responses << {
+        command: "bc <<< #{arg1}^#{power}",
+        explanation: "Calculates #{power_text} of #{arg1}"
+      }
+    return responses
+    end
+    
+    
+    matches = command.match(/^what\s+is\s+(.+)\s+(.+)\s(.+)$/)
     if matches
       if (matches[1].numeric? && matches[3].numeric?)
         arg1 = matches[1]
@@ -43,7 +87,7 @@ module Calculate
     commands << {
       category: "Calculate",
       description: '\033[34mCalculate\033[0m',
-      usage: ["- betty what is 40 plus 2"]
+      usage: ["- betty what is 40 plus 2", "- betty what is the square root of 123", "- betty what is the cube of 3" ]
     }
     commands
   end
