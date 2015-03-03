b5735ff201c24c46c434bd3cad3f78cf34d96d96
NisarAhmad- <nisarahmad1324@gmail.com>
2015-03-03 11:17:32 +0530

    added shutdown module

--- lib/shutdown.rb
+++ lib/shutdown.rb
@@ -14,7 +14,7 @@ module Shutdown
       flags = command.match(/shutdown/) ? '-h' : '-r' ##shutdown or restart
       responses << {
         :command => "sudo shutdown #{flags} #{option_when}",
-        :explanation => "restart/shutdown system"
+        :explanation => "restart/shutdown system #{option_when}"
       }
     end
     if command.match(/^(?:restart|reboot|shutdown)\s+(?:system|computer)\s+?(?:at\s+)?(\d{1,2}:\d{1,2})$/)
@@ -22,41 +22,37 @@ module Shutdown
       flags = command.match(/shutdown/) ? '-h' : '-r' ##shutdown or restart
       responses << {
         :command => "sudo shutdown #{flags} #{shutdown_time}",
-        :explanation => "restart/shutdown system"
+        :explanation => "restart/shutdown system at #{shutdown_time}"
       }
     end
-    if command.match(/^(?:cancel|abort)\s+(?:system|computer)\s+(?:restart|reboot|shutdown)$/)
-      responses << {
-        :command => "sudo shutdown -c",
-        :explanation => "cancel system restart/shutdown."
-      }
-    end
-
-    if act = command.match(/^(?:shutdown)\s+(?:system|computer|pc)\s+?(?:when\s+)?(.* || [0123456789])?\s*(exits|completes)?$/)
+    if act = command.match(/^(?:shutdown)\s+(?:system|computer|pc)\s+?(?:(when|after)\s+)?(.* || [0123456789])?\s*(exits|exit|completes|finishes|finish)?$/)
       action = act.to_s.split
       request = "sudo shutdown -h now"
       process = action[3] 
       `sudo echo "got root "` #getting root now to avoid getting password prompt later
-      puts "waiting for #{process} to exit"
+      puts " *** waiting for #{process} to exit ***"
+      puts " (you can minimize this window but don't close it)"
       count = 0
       while true
         processes = `ps -e`
         is_running = processes.scan(process.downcase)
         if is_running.size == 0 && count == 0
-          request = "echo \"#{process} is NOT running\""
+          request = "echo \"\nERROR : #{process} is NOT running\""
+          conseq = "prevent shutdown accidentally"
           count += 1
           break
         elsif is_running.size == 0 && count != 0
           request = "sudo shutdown -h now"
+          conseq = "shutdown system when #{process} exits"
           break
-        else is_running.size ==1
+        else is_running.size > 0
         end
         count += 1
         sleep(1)
       end
       responses << {
         :command => "#{request}",
-        :explanation => "shutdown system when any given process exits"
+        :explanation => "#{conseq}"
       }
     end
     responses
@@ -66,7 +62,7 @@ module Shutdown
     commands << {
       :category => "Shutdown",
       :description => 'Shutdown / reboot computer right now or in a future time',
-      :usage => ["shutdown computer now", "restart computer in 60 min", "reboot system at 00:01","shutdown computer when apt-get finishes","shutdown computer when firefox exits"]
+      :usage => ["shutdown computer now", "restart computer in 60 min", "reboot system at 00:01","shutdown computer when wget finishes","shutdown computer when firefox exits"]
     }
     commands
 
